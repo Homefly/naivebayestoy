71327741940630eeed3c7306dde06c5c45f206dd
Daniel Kuddes <daniel.kuddes@gmail.com>
2019-03-09 23:49:09 -0500

    functionalized geting sensor returns

--- tf_radar+ir_attr.py
+++ tf_radar+ir_attr.py
@@ -57,7 +57,7 @@ class TFNaiveBayesClassifier:
 class StatMethods:
 
     @staticmethod
-    def inverseVarFusion(obs1, obs2, sD1, sD2):
+    def inverseVarMean(obs1, obs2, sD1, sD2):
         #gets pass two arrays of observations to combine into a new array
         #sD1, sD2 are single floats
         var1 = sD1**2.0
@@ -69,82 +69,88 @@ class StatMethods:
         #import ipdb; ipdb.set_trace()
         for ob1, ob2 in zip(obs1, obs2):
             newObs.append(ob1*weight1 +ob2*weight2)
-
         return newObs
 
+    @staticmethod
+    def inverseVarSD(sD1, sD2):
+        fusedSD = 1/(sD1**(-1) + sD2**(-1))
+        return fusedSD
+
+
 class GenSigs:
+
+    @staticmethod
+    def getSenReturns(numSamples, hostile, friendly):
+
+        # Create simulated radar returns and formate them for tensor flow
+        hSizesRadar = GenSigs.drawFromNormDist(
+                numSamples, mean = hostile ['size'],
+                stddev = hostile ['sizeRadarSD'], seed = 1234)
+        hTempRadar  = GenSigs.drawFromNormDist(
+                numSamples, mean = hostile ['temp'], 
+                stddev = hostile ['tempRadarSD'], seed = 1235)
+        radarReturnsHostile = np.array(
+                [[size, temp] for size, temp in zip(hSizesRadar, hTempRadar)], 
+                dtype = 'float64')
+        fSizesRadar = GenSigs.drawFromNormDist(
+                numSamples, mean = friendly['size'],
+                stddev = friendly['sizeRadarSD'], seed = 1236)
+        fTempRadar  = GenSigs.drawFromNormDist(
+                numSamples, mean = friendly['temp'], 
+                stddev = friendly['tempRadarSD'], seed = 1237)
+        radarReturnsFriendly = np.array(
+                [[size, temp] for size, temp in zip(fSizesRadar, fTempRadar)], 
+                dtype = 'float64')
+        xRadar = np.append(radarReturnsHostile, radarReturnsFriendly, axis = 0)
+        
+        # Create simulated ir returns and formate them for tensor flow
+        hSizesIR = GenSigs.drawFromNormDist(numSamples, mean = hostile ['size'],
+                                            stddev = hostile ['sizeIRSD'],
+                                            seed = 1238)
+        hTempIR  = GenSigs.drawFromNormDist(numSamples, mean = hostile ['temp'],
+                                            stddev = hostile ['tempIRSD'],
+                                            seed = 1239)
+        irReturnsHostile = np.array(
+                [[size, temp] for size, temp in zip(hSizesIR, hTempIR)], 
+                dtype = 'float64')
+        fSizesIR = GenSigs.drawFromNormDist(numSamples, mean = friendly['size'], 
+                                            stddev = friendly['sizeIRSD'], 
+                                            seed = 1240)
+        fTempIR  = GenSigs.drawFromNormDist(numSamples, mean = friendly['temp'],
+                                            stddev = friendly['tempIRSD'], 
+                                            seed = 1241)
+        irReturnsFriendly = np.array(
+                [[size, temp] for size, temp in zip(fSizesIR, fTempIR)], 
+                dtype = 'float64')
+        xIR = np.append(irReturnsHostile, irReturnsFriendly, axis = 0)
+        
+        return xRadar, xIR
 
     @staticmethod
     def drawFromNormDist(numSamples, mean, stddev, seed):
         sess = tf.Session()
         draws = sess.run(tf.random_normal([1, numSamples], mean=mean,
                          stddev=stddev, seed = seed))[0]
-
         return draws
 
 
 if __name__ == '__main__':
 
-    # Create hostile radar
+    #Gets radar and ir returns
     numSamples = 10
- 
-    # Create simulated radar returns and formate them for tensor flow
-    hSizesRadar = GenSigs.drawFromNormDist(numSamples, mean = hostile ['size'],
-                                            stddev = hostile ['sizeRadarSD'],
-                                            seed = 1234)
-    hTempRadar  = GenSigs.drawFromNormDist(numSamples, mean = hostile ['temp'], 
-                                           stddev = hostile ['tempRadarSD'],
-                                           seed = 1235)
-    radarReturnsHostile = np.array(
-            [[size, temp] for size, temp in zip(hSizesRadar, hTempRadar)], 
-            dtype = 'float64')
-    fSizesRadar = GenSigs.drawFromNormDist(numSamples, mean = friendly['size'],
-                                           stddev = friendly['sizeRadarSD'], 
-                                           seed = 1236)
-    fTempRadar  = GenSigs.drawFromNormDist(numSamples, mean = friendly['temp'], 
-                                           stddev = friendly['tempRadarSD'], 
-                                           seed = 1237)
-    radarReturnsFriendly = np.array(
-            [[size, temp] for size, temp in zip(fSizesRadar, fTempRadar)], 
-            dtype = 'float64')
-    
-    # Create simulated ir returns and formate them for tensor flow
-    hSizesIR = GenSigs.drawFromNormDist(numSamples, mean = hostile ['size'],
-                                        stddev = hostile ['sizeIRSD'],
-                                        seed = 1238)
-    hTempIR  = GenSigs.drawFromNormDist(numSamples, mean = hostile ['temp'],
-                                        stddev = hostile ['tempIRSD'],
-                                        seed = 1239)
-    irReturnsHostile = np.array(
-            [[size, temp] for size, temp in zip(hSizesIR, hTempIR)], 
-            dtype = 'float64')
-    fSizesIR = GenSigs.drawFromNormDist(numSamples, mean = friendly['size'], 
-                                        stddev = friendly['sizeIRSD'], 
-                                        seed = 1240)
-    fTempIR  = GenSigs.drawFromNormDist(numSamples, mean = friendly['temp'],
-                                        stddev = friendly['tempIRSD'], 
-                                        seed = 1241)
-    irReturnsFriendly = np.array(
-            [[size, temp] for size, temp in zip(fSizesIR, fTempIR)], 
-            dtype = 'float64')
+    xRadar, xIR = GenSigs.getSenReturns(numSamples, hostile, friendly)
 
-    xRadar = np.append(radarReturnsHostile, radarReturnsFriendly, axis = 0)
-    xIR = np.append(irReturnsHostile, irReturnsFriendly, axis = 0)
-    
-    s = tf.Session()
-
-    fuseA1 = StatMethods.inverseVarFusion(xRadar[:,0], xIR[:,0], 
+    #Fuse attributes using inverseVar result is MAP
+    fuseSize = StatMethods.inverseVarMean(xRadar[:,0], xIR[:,0], 
             friendly['sizeRadarSD'], friendly['sizeIRSD'])
-    fuseA2 = StatMethods.inverseVarFusion(xRadar[:,1], xIR[:,1], 
+    fuseTemp = StatMethods.inverseVarMean(xRadar[:,1], xIR[:,1], 
             friendly['tempRadarSD'], friendly['tempIRSD'])
+    X = np.array([[att1, att2] for att1, att2 in zip(fuseSize, fuseTemp)])
 
-    X = np.array([[att1, att2] for att1, att2 in zip(fuseA1, fuseA2)])
-
-    fuseSizeSD = 1/(hostile['sizeRadarSD']**(-1) + hostile['sizeIRSD']**(-1))
-    fuseTempSD = 1/(hostile['tempRadarSD']**(-1) + hostile['tempIRSD']**(-1))
-
-    y1 = np.ones(10)
-    y2 = np.zeros(10)
+    fuseSizeSD = StatMethods.inverseVarSD(hostile['sizeRadarSD'], 
+                                          hostile['sizeIRSD'])
+    fuseTempSD = StatMethods.inverseVarSD(hostile['tempRadarSD'], 
+                                          hostile['tempIRSD'])
 
     mean = np.array(
             [[hostile['size'], hostile['temp']],
@@ -153,16 +159,15 @@ if __name__ == '__main__':
     sD =  np.array([[fuseSizeSD, fuseTempSD],[fuseSizeSD, fuseTempSD]])
 
     tf_nb = TFNaiveBayesClassifier()
-    #tf_nb.fit(X, y)
     tf_nb.defineClasses(mean, sD)
-    
+
     # Create a regular grid and classify each point
     x_min, x_max = X[:, 0].min() - 10, X[:, 0].max() + 10
     y_min, y_max = X[:, 1].min() - 10, X[:, 1].max() + 10
-
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, 300),
                          np.linspace(y_min, y_max, 300))
-    #s = tf.Session()
+    
+    s = tf.Session()
     Z = s.run(tf_nb.predict(np.c_[xx.ravel(), yy.ravel()]))
 
     # Extract probabilities of class 2 and 3
@@ -171,7 +176,6 @@ if __name__ == '__main__':
     # Plot
     fig = plt.figure(figsize=(5, 3.75))
     ax = fig.add_subplot(111)
-
 
     points = ax.scatter(x = X[:10,0], y=X[:10, 1], c='red', edgecolor='k', 
                         label = 'Hostile')
@@ -192,8 +196,6 @@ if __name__ == '__main__':
     ax.set_title('Radar and IR Attribute Decision Boundary')
     ax.set_xlim(x_min, x_max)
     ax.set_ylim(y_min, y_max)
-    #ax.set_xticks(())
-    #ax.set_yticks(())
 
     ax.legend()
 
