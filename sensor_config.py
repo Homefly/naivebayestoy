c1f9902bc63870d262c3fff364f7df36a268e8a9
Daniel Kuddes <daniel.kuddes@gmail.com>
2019-03-09 21:01:48 -0500

    added 2 files

---
+++ sensor_config.py
@@ -0,0 +1,17 @@
+hostile =   {
+            'size':10, #meters
+            'sizeRadarSD':13.0, #meters
+            'sizeIRSD':20.0, #meters
+            'temp':100.0, #C
+            'tempRadarSD':100.0, #C
+            'tempIRSD':20.0 #C
+            }
+
+friendly =  {
+            'size':30, #meters
+            'sizeRadarSD':13.0, #meters
+            'sizeIRSD':20.0, #meters
+            'temp':80, #C
+            'tempRadarSD':100.0, #C
+            'tempIRSD':20.0 #C
+            }
